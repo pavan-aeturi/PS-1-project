2ba4b2228019c88cee2dc1e3aa30cd100f3952b8
pavan-aeturi <f20180212@goa.bits-pilani.ac.in>
2020-06-18 18:30:42 +0530

    references changed

--- src/ScreenNav.js
+++ src/ScreenNav.js
@@ -40,9 +40,9 @@ class ScreenNav extends Component {
                   <Navbar.Toggle aria-controls="basic-navbar-nav" />
                   <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                      <Nav.Link href="/">Home</Nav.Link>
-                      <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-                      <Nav.Link href="/stock">Analyse</Nav.Link>
+                      <Nav.Link href="./">Home</Nav.Link>
+                      <Nav.Link href="./portfolio">Portfolio</Nav.Link>
+                      <Nav.Link href="./stock">Analyse</Nav.Link>
                     </Nav>
                     {this.state.presentAddress === "/stock" && (
                       <Search
