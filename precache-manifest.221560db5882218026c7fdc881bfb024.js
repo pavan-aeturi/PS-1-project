e83e0e86178b57749cbf3e9c7937cf36a83f97e0
pavan-aeturi <f20180212@goa.bits-pilani.ac.in>
2020-06-18 10:08:40 +0530

    Updates

---
+++ precache-manifest.221560db5882218026c7fdc881bfb024.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb62d30213d710d5aed3d2792a142df",
+    "url": "/PS-1-project/index.html"
+  },
+  {
+    "revision": "f4761ae5aadf58c6aefd",
+    "url": "/PS-1-project/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "20599bd1729056f3beea",
+    "url": "/PS-1-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f4761ae5aadf58c6aefd",
+    "url": "/PS-1-project/static/js/2.6d6d49a9.chunk.js"
+  },
+  {
+    "revision": "1bfda9b2e15533b32cb94c86916b021d",
+    "url": "/PS-1-project/static/js/2.6d6d49a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20599bd1729056f3beea",
+    "url": "/PS-1-project/static/js/main.27b147ba.chunk.js"
+  },
+  {
+    "revision": "59045858dab476b91fde",
+    "url": "/PS-1-project/static/js/runtime-main.472ba519.js"
+  }
+]);
