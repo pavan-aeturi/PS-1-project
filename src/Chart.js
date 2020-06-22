e7f5dce7130772812e26fb83f3a644c0a70151d8
pavan-aeturi <f20180212@goa.bits-pilani.ac.in>
2020-06-23 03:29:54 +0530

    trailing of realtime stock price

--- src/Chart.js
+++ src/Chart.js
@@ -1,5 +1,25 @@
 import React from "react";
 import Plot from "react-plotly.js";
+const https = require("https");
+
+const options = {
+  hostname: "financialmodelingprep.com",
+  port: 443,
+  path: "/api/v3/quote-short/AAPL",
+  method: "GET",
+};
+
+const req = https.request(options, (res) => {
+  res.on("data", (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on("error", (error) => {
+  console.error(error);
+});
+
+req.end();
 class Chart extends React.Component {
   render() {
     return (
